be149c9ea82c5be1f4fc8d13093a41247ef8edfa
Simone823 <simone.daglio823@outlook.it>
2022-05-03 23:42:48 +0200

    modifica view welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Welcome</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -12,8 +12,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
-                color: #636b6f;
+                background-color: #343a40;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
                 height: 100vh;
@@ -30,32 +29,44 @@
                 justify-content: center;
             }
 
-            .position-ref {
-                position: relative;
+            .flex-column {
+                flex-direction: column;
             }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
+            .row-gap-25 {
+                row-gap: 25px;
             }
 
-            .content {
+            .text-center {
                 text-align: center;
             }
 
-            .title {
-                font-size: 84px;
+            .text-uppercase {
+                text-transform: uppercase;
+            }
+
+            .links > a:not(:last-child) {
+                margin-bottom: 15px;
             }
 
             .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
+                color: rgb(182, 179, 179);
+                font-size: 16px;
                 font-weight: 600;
                 letter-spacing: .1rem;
                 text-decoration: none;
                 text-transform: uppercase;
+                display: block;
+            }
+
+            .links > a:hover {
+                color: white;
+                transition: color 300ms linear;
+            }
+
+            .links > p {
+                color: rgb(182, 179, 179);
+                font-weight: bold;
             }
 
             .m-b-md {
@@ -64,13 +75,26 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+        <div class="flex-center full-height flex-column row-gap-25">
+            <div class="logo">
+                <svg xmlns="http://www.w3.org/2000/svg" width="120px" height="120px" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" width="512" height="512" x="0" y="0" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512" xml:space="preserve" class=""><g><g xmlns="http://www.w3.org/2000/svg"><g><g><g><g><g id="XMLID_670_"><g id="XMLID_671_"><g id="XMLID_672_"><g id="XMLID_673_"><g id="XMLID_674_"><g id="XMLID_675_"><g id="XMLID_676_"><g id="XMLID_677_"><g id="XMLID_678_"><g id="XMLID_679_"><g id="XMLID_680_"><g id="XMLID_681_"><g id="XMLID_682_"><g id="XMLID_696_"><g id="XMLID_697_"><circle cx="256" cy="256" fill="#52525b" r="256" data-original="#52525b" class=""></circle>
+                    </g><g><path d="m68.8 405.501 85.504 85.504c31.172 13.508 65.56 20.995 101.696 20.995 141.385 0 256-114.615 256-256 0-5.366-.165-10.693-.491-15.977l-68.31-68.31-14.905 19.096-140.254-140.254-114.804 99.99z" fill="#2c2c33" data-original="#2c2c33" class=""></path></g></g></g></g></g></g></g></g></g></g></g></g></g></g></g></g></g></g></g><g><g><path d="m393.888 235.332-137.888-148.93-137.888 148.93-22.013-20.381 159.901-172.707 159.901 172.707z" fill="#ffffff" data-original="#ffffff"></path></g><path d="m393.888 235.333 22.013-20.382-159.901-172.707v44.158z" fill="#e0e0e0" data-original="#e0e0e0" class=""></path><path d="m213.79 78.019c0-23.312 18.898-42.21 42.21-42.21 0 0 28.108 18.898 28.108 42.21s-28.108 42.21-28.108 42.21c-23.312 0-42.21-18.898-42.21-42.21z" fill="#80d261" data-original="#80d261" class=""></path><g><g><g><path d="m298.21 78.019c0-23.312-18.898-42.21-42.21-42.21v84.42c23.312 0 42.21-18.898 42.21-42.21z" fill="#08b865" data-original="#08b865" class=""></path></g></g></g><path d="m443.2 405.501h-374.4v-50.313l19.468-66.581-19.468-66.581v-50.313h374.4v50.313l-11.486 66.581 11.486 66.581z" fill="#ffe029" data-original="#ffe029" class=""></path>
+                    <g><path d="m443.2 171.713h-187.2v233.788h187.2v-50.313l-11.486-66.581 11.486-66.581z" fill="#ffad1d" data-original="#ffad1d"></path></g><path d="m68.8 222.026h374.399v133.162h-374.399z" fill="#ffffff" data-original="#ffffff"></path><g><path d="m256 222.026h187.2v133.162h-187.2z" fill="#e0e0e0" data-original="#e0e0e0" class=""></path></g><g><g><g><g><g><path d="m382.165 314.88h-14.548l-3.409-17.414-4.703 16.824h-12.397l-4.951-16.767-3.123 17.357h-14.51l9.341-51.937h12.977l6.265 21.213 5.922-21.185 12.977-.048z" fill="#2c2c33" data-original="#2c2c33" class=""></path></g></g></g><g><g><path d="m221.753 314.847h-27.675v-52.497h14.282v38.216h13.393z" fill="#52525b" data-original="#52525b" class=""></path></g></g><path d="m188.361 276.981v-14.282h-30.251v52.181h30.251v-14.282h-15.969v-4.668h14.792v-14.282h-14.792v-4.667z" fill="#52525b" data-original="#52525b" class=""></path><g><path d="m416.101 276.981v-14.282h-30.25v52.181h30.25v-14.282h-15.969v-4.668h14.792v-14.282h-14.792v-4.667z" fill="#2c2c33" data-original="#2c2c33" class=""></path></g><g><g>
+                    <path d="m249.371 314.88c-14.483 0-26.265-11.782-26.265-26.265 0-14.482 11.782-26.265 26.265-26.265 5.262 0 10.342 1.553 14.69 4.49l5.917 3.997-7.994 11.835-5.917-3.997c-1.978-1.336-4.293-2.043-6.695-2.043-6.608 0-11.983 5.376-11.983 11.983 0 6.608 5.376 11.983 11.983 11.983 2.364 0 4.244-.651 5.747-1.989.291-.258.57-.547.829-.857l4.588-5.472 10.944 9.176-4.588 5.472c-.704.839-1.469 1.628-2.275 2.346-4.118 3.667-9.39 5.606-15.246 5.606z" fill="#52525b" data-original="#52525b" class=""></path></g></g><g><g><path d="m154.787 262.695-10.349 52.023-12.854.086-6.255-21.689-6.246 21.603-12.873.086-10.311-52.109h14.567l3.571 18.062 5.093-17.624 12.425.019 5.075 17.624 3.599-18.081z" fill="#52525b" data-original="#52525b" class=""></path></g></g><g><g><path d="m298.318 314.863c-14.482 0-26.264-11.782-26.264-26.265s11.783-26.265 26.264-26.265c14.483 0 26.265 11.782 26.265 26.265s-11.782 26.265-26.265 26.265zm0-38.248c-6.607 0-11.983 5.376-11.983 11.983s5.376 11.983 11.983 11.983c6.608 0 11.983-5.376 11.983-11.983s-5.375-11.983-11.983-11.983z" fill="#2c2c33" data-original="#2c2c33" class=""></path></g></g></g></g></g><g><g fill="#2c2c33">
+                    <path d="m256.066 278.674 5.917 3.997 7.994-11.834-5.917-3.997c-2.483-1.677-5.204-2.903-8.061-3.646v15.436c.023.015.045.029.067.044z" fill="#2c2c33" data-original="#2c2c33" class=""></path>
+                    <path d="m260.535 292.28-4.535 5.409v16.297c3.21-.906 6.125-2.492 8.616-4.711.806-.718 1.571-1.507 2.275-2.346l4.588-5.472z" fill="#2c2c33" data-original="#2c2c33" class=""></path>
+                    </g></g></g></g>
+                </svg>
+            </div>
+
             @if (Route::has('login'))
-                <div class="top-right links">
+                <div class="links text-center">
                     @auth
                         {{-- <a href="{{ route('admin.posts.index')}}">Home</a> --}}
                     @else
                         <a href="{{ route('login') }}">Login</a>
+
+                        <p class="text-uppercase">Or</p>
 
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}">Register</a>
@@ -78,23 +102,6 @@
                     @endauth
                 </div>
             @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
         </div>
     </body>
 </html>
